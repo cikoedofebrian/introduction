fea1369f3d5da79328265e4bab179e7fd221bc51
Ciko Edo Febrian <94286488+cikoedofebrian@users.noreply.github.com>
2021-11-14 17:13:33 +0700

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,11 @@
+sp = input("Who's your name? :")
+
+print(f"Hello {sp}, my name is Ciko Edo Febrian,")
+print(f"i'm currently studying at The University of Jember in Technology Information major.")
+con = input(f"{sp}, Would you like to know more about me?\nChoose 'y' if yes, choose 'no' if no =")
+
+if con == "y" :
+    print("I really like playing games, such as Genshin Impact, Harvest Moon, etc.")
+    print("I wish i can be a game developer in the future.")
+else :
+    print(f"Aww, you seem uninterested with me, that's fine!\nSee you later, {sp}.")
